6a0aa4766b80d96c6bd75bc23fb79f7e8ae078a8
prabhatsingh985 <ps2656728@gmail.com>
2024-10-03 12:23:10 +0530

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: "/coursera-paradise-nursery-shopping-application/",
+  plugins: [react()],
+})
